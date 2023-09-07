bc0a611924c6ed0ce89c58f32caed81b3942a0f6
Stefan Eng <eng_steff@outlook.com>
2023-09-07 21:26:49 +0200

    feat: Include timestamp in LOG macro

--- src/ctest.c
+++ src/ctest.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <time.h>
 
-#define LOG(LEVEL, ...)         \
+#define SIZE_TIMESTAMP 30
+char TIMESTAMP[SIZE_TIMESTAMP] = {0};
+time_t time_raw = {0};
+struct tm * time_info = NULL;
+
+char *
+timestamp(void)
+{
+  time_raw = time(NULL);
+  time_info = localtime(&time_raw);
+  strftime(TIMESTAMP, SIZE_TIMESTAMP, "%Y-%m-%d %H:%M:%S", time_info);
+  return TIMESTAMP;
+}
+
+#define LOG(LEVEL, fmt, ...)         \
   do {                          \
     if (DEBUG_LEVEL <= LEVEL) { \
-      printf(__VA_ARGS__);      \
+      printf("%s - " fmt, timestamp(), __VA_ARGS__);      \
     }                           \
   } while(0)
 
@@ -23,5 +38,5 @@ enum LOG_LEVEL {
 int
 main(int argc, char ** argv)
 {
-  LOG(LOG_DEBUG, "START OF MAIN\n");
+  LOG(LOG_DEBUG, "%s\n", "START OF MAIN");
 }
