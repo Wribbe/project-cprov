8c40aec62e92e25276b84e9ee55e2707965df4af
Stefan Eng <eng_steff@outlook.com>
2023-10-19 21:49:56 +0200

    feat: Expand ctest_gen with includes, head and tail

--- src/ctest_gen.c
+++ src/ctest_gen.c
@@ -1,12 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const char * includes = \
+ "#include <stdlib.h>\n"
+ "#include <stdio.h>\n";
+
+const char * head = \
+  "int\n"
+  "main (void)\n"
+  "{\n";
+
+const char * tail = \
+  "}\n";
+
 int
 main(int argc, const char ** argv)
 {
-  printf("%s\n", "int");
-  printf("%s\n", "main(void)");
-  printf("%s\n", "{");
-  printf("%s\n", "  return 1;");
-  printf("%s\n", "}");
+  printf("%s\n", includes);
+  printf("%s", head);
+  printf("%s\n", "  printf(\"%s\\n\", \"HELLO WORLD\");");
+  printf("%s", tail);
 }
